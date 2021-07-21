2a0c515e6edf5f14a38dd90edae2fb9642dca1a3
superhackermans <79132305+superhackermans@users.noreply.github.com>
2021-07-21 04:53:04 -0700

    got rid of spaces

--- slowzoom.py
+++ slowzoom.py
@@ -12,7 +12,7 @@ def slow_zoom(suffix, clips, directory):
     last_clip_loc = f"{directory}{cam}{clips[-1][0]}{suffix}"
 
     tempfile = f"{directory}{cam}{clips[0][0]}TEMP{suffix}"
-    total_frames = float(get_length(first_clip_loc)) * frameRate
+    total_frames = get_packets(first_clip_loc)
     zoom_in_cmd = f"""-vf "scale=w=(3840*4):h=(2160*4), zoompan=z='min(pzoom+({zoom_in}-1)/{total_frames},{zoom_in})':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':s=3840x2160" """
     command = f"""ffmpeg -y -i {first_clip_loc} {zoom_in_cmd} -an {tempfile} -hide_banner -loglevel error"""
     subprocess.call(command, shell=True)
@@ -27,8 +27,10 @@ def slow_zoom(suffix, clips, directory):
     deleteFile(f"{directory}{cam}{clips[0][0]}.wav")
     deleteFile(tempfile)
 
+
+
     tempfile = f"{directory}{cam}{clips[-1][0]}TEMP{suffix}"
-    total_frames = float(get_length(last_clip_loc)) * frameRate
+    total_frames = get_packets(last_clip_loc)
     zoom_in_cmd = f"""-vf "scale=w=(3840*4):h=(2160*4), zoompan=z='min(pzoom+({zoom_in}-1)/{total_frames},{zoom_in})':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':s=3840x2160" """
     command = f"""ffmpeg -y -i {last_clip_loc} {zoom_in_cmd} -an {tempfile} -hide_banner -loglevel error"""
     subprocess.call(command, shell=True)
