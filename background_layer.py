5bcfa1bb16295185f570458f737e7678dd91795f
superhackermans <79132305+superhackermans@users.noreply.github.com>
2021-07-20 22:36:08 -0700

    moved out group consec function

--- background_layer.py
+++ background_layer.py
@@ -16,20 +16,6 @@ def convertfile(suffix, newsuffix, clips, directory):
 
             deleteFile(filename)
 
-def group_consecutives(vals, step=1):
-    """Return list of consecutive lists of numbers from vals (number list)."""
-    run = []
-    result = [run]
-    expect = None
-    for v in vals:
-        if (v == expect) or (expect is None):
-            run.append(v)
-        else:
-            run = [v]
-            result.append(run)
-        expect = v + step
-    return result
-
 def add_transparency(suffix, newsuffix, clips, directory):
     # print("Adding transparency to top layer files (OUTPUT/trimmed_files/)")
     createPath(wav_dir)
