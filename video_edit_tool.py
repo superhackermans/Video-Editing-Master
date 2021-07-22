ad7fd6879176659dda451c40b6cef242a6ce1402
superhackermans <79132305+superhackermans@users.noreply.github.com>
2021-07-21 22:03:06 -0700

    functional

--- video_edit_tool.py
+++ video_edit_tool.py
@@ -10,16 +10,16 @@ from alternate_zoom import *
 from zoomandfade import slow_zoom, fade_out
 
 if __name__ == '__main__':
-    # make_folders()
-    # trimmer(filesuffix, vid_dir_in) # desired output, and directory
-    # splitcovers(cov_dir_in)
-    #
-    # dup_dir(layer2, backuplayer)
-    # dup_dir(layer2, layer3)
-    # dup_dir(layer2, layer4)
-    #
-    reset()
-    #
+    make_folders()
+    trimmer(filesuffix, vid_dir_in) # desired output, and directory
+    splitcovers(cov_dir_in)
+
+    dup_dir(layer2, backuplayer)
+    dup_dir(layer2, layer3)
+    dup_dir(layer2, layer4)
+
+    # reset()
+
     concat_and_replace(filesuffix, filesuffix, clips_all_except_pics_and_vid, layer2, vid_transparency_smol)
     concat_and_replace(filesuffix, filesuffix, clips_background, layer3, backgroundloc)
     concat_and_replace(filesuffix, filesuffix, clips_ben_and_cover, layer3, vid_transparency_smol)
@@ -31,17 +31,14 @@ if __name__ == '__main__':
     add_transparency(filesuffix, filesuffix, clips_all, layer0)
     dup_dir(layer0, layer1)
 
-    #covers
     attachcovers(filesuffix, clips_cover, layer1)
     attachsidecovers(filesuffix, clips_cover, layer1)
     concat_and_replace(filesuffix, filesuffix, clips_all_except_cover, layer1, vid_transparency_smol)
 
-    #transitions
     transitions(filesuffix, clips_background, layer0)
     concat_and_replace(filesuffix, filesuffix, clips_background, layer0, vid_transparency_smol)
     concat_and_replace(filesuffix, filesuffix, clips_ben, layer0, vid_transparency_smol)
 
-    #zooms and fades
     altzoom(filesuffix, clips_ben, layer4)
     slow_zoom(filesuffix, clips_ben, layer4)
     fade_out(filesuffix, clips_ben, layer4)
