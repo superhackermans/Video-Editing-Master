0bf298feaa5bab9854ecf1afd4a87eaa200fe330
superhackermans <79132305+superhackermans@users.noreply.github.com>
2021-10-28 20:17:30 -0700

    renumbering algorithm

--- Renumber.py
+++ Renumber.py
@@ -1,17 +1,29 @@
 from parameters import *
 
-def renumber(suffix, original_number, replacement_number):
+def renumber(directory, suffix, original_number, first_replace, last_replace):
     myVideos = []
     for file in os.listdir(directory):
         if file.endswith(".MP4") or file.endswith(".mp4"):
             myVideos.append(file)
 
-    os.rename(src, dst)
 
+    def clip(number):
+        clip_loc = f"{directory}{cam}{str(number).zfill(4)}{suffix}"
+        return clip_loc
 
-first_clip_loc = f"{directory}{cam}{str(clips[0][0]).zfill(4)}{suffix}"
+    original_clip = clip(original_number)
+    temp_clip =  clip(0000)
+
+    os.rename(original_clip, temp_clip)
+
+    while clip(first_replace) != clip(last_replace):
+        os.rename(clip(last_replace), clip(last_replace+1))
+        last_replace -= 1
+
+    os.rename(clip(first_replace), clip(first_replace+1))
+    os.rename(temp_clip, clip(first_replace))
 
 
 if __name__ == '__main__':
-    renumber(".MP4", 100, 50)
-    # 100 -> 50 and 50 -> 51 and so forth
+    renumber(vid_dir_in, ".MP4", 717, 706, 715)
+    # 717 -> 706 and 706 -> 707 and so forth
